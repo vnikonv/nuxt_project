fd8e63031228dd8fd81135aafa7b917d4352ae0c
Nikon Veremeichik <thesecondnikon@gmail.com>
2025-03-10 22:11:10 +0500

    Fixed burger

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import {useHead} from '#app';
+import { useHead } from '#app';
 import {ref} from 'vue';
 
 useHead({
@@ -25,8 +25,8 @@ useHead({
 
 const burger = ref<boolean>(false);
 const submenu = ref<boolean>(false);
-const switch_burger = ()=> {burger.value = !burger.value};
-const switch_submenu = ()=> {submenu.value = !submenu.value};
+const switch_burger = () => {burger.value = !burger.value};
+const switch_submenu = () => {submenu.value = !submenu.value};
 </script>
 
 <template>
@@ -36,28 +36,30 @@ const switch_submenu = ()=> {submenu.value = !submenu.value};
         </div>
         <div class = "basis-1/4 max-sm:basis-auto">
         </div>
-        <nav :class="{'basis-1/2 flex flex-row items-center justify-end px-8 gap-4': !burger || burger, 'max-sm:basis-auto max-sm:absolute max-sm:top-full max-sm:w-full max-sm:justify-center max-sm:flex-col max-sm:gap-0 max-sm:bg-white max-sm:px-0': burger, 'max-sm:hidden': !burger}">
+        <nav :class="{'basis-1/2 flex flex-row items-center justify-end px-4 gap-4' : !burger || burger, 
+        'max-sm:basis-auto max-sm:absolute max-sm:top-full max-sm:w-full max-sm:justify-center max-sm:flex-col max-sm:gap-0 max-sm:px-0' : burger,
+        'max-sm:hidden' : !burger}">
             <NuxtLink to = "/" class = "my-auto p-2 hover:bg-gray-500 hover:text-white rounded-lg max-sm:border-b-2 max-sm:w-full max-sm:border-black max-sm:text-center">Home</NuxtLink>
             <div class = "my-auto p-2 hover: hover:bg-gray-500 rounded-lg hover:text-white max-sm:w-full max-sm:text-center max-sm:px-0 max-sm:pb-0" @click = "switch_submenu">Labs
                 <div class = "flex flex-col top-2 absolute bg-white text-black w- text-center max-sm:relative max-sm:w-full max-sm:bg-red-100 max-sm:px-0" v-show="submenu">
-                    <NuxtLink to =/lab3 class="my-auto p-2 border-b-2 rounded-lg border-black hover:bg-gray-500 hover:text-white max-sm:w-full max-sm:text-center">Lab3</NuxtLink>
-                    <NuxtLink to =/lab4 class="my-auto p-2 border-b-2 rounded-lg border-black hover:bg-gray-500 hover:text-white max-sm:w-full max-sm:text-center">Lab4</NuxtLink>
-                    <NuxtLink to =/lab5 class="my-auto p-2 border-b-2 rounded-lg border-black hover:bg-gray-500 hover:text-white max-sm:w-full max-sm:text-center">Lab5</NuxtLink>
-                    <NuxtLink to =/lab6 class="my-auto p-2 border-b-2 rounded-lg border-black hover:bg-gray-500 hover:text-white max-sm:w-full max-sm:text-center">Lab6</NuxtLink>
+                    <NuxtLink to =/lab3 class="my-auto p-2 border-b-2 rounded-lg border-black hover:bg-gray-500 hover:text-white max-sm:w-full max-sm:text-center max-sm:bg-slate-200">Lab3</NuxtLink>
+                    <NuxtLink to =/lab4 class="my-auto p-2 border-b-2 rounded-lg border-black hover:bg-gray-500 hover:text-white max-sm:w-full max-sm:text-center max-sm:bg-slate-200">Lab4</NuxtLink>
+                    <NuxtLink to =/lab5 class="my-auto p-2 border-b-2 rounded-lg border-black hover:bg-gray-500 hover:text-white max-sm:w-full max-sm:text-center max-sm:bg-slate-200">Lab5</NuxtLink>
+                    <NuxtLink to =/lab6 class="my-auto p-2 border-b-2 rounded-lg border-black hover:bg-gray-500 hover:text-white max-sm:w-full max-sm:text-center max-sm:bg-slate-200">Lab6</NuxtLink>
                 </div>
             </div>
             <NuxtLink to = "/login" :class="{'my auto p-2 hover:bg-gray-500 hover:text-white rounded-lg max-sm:border-b-2 max-sm:w-full max-sm:border-black max-sm:text-center': submenu,'my-auto p-2 rounded-lg hover:bg-gray-500 hover:text-white max-sm:w-full max-sm:border-b-2 max-sm:text-center':!submenu}">Login</NuxtLink>
             <NuxtLink to = "/logout" class="my auto p-2 hover:bg-gray-500 hover:text-white rounded-lg max-sm:border-b-2 max-sm:w-full max-sm:border-black max-sm:text-center">Logout</NuxtLink>
         </nav>
-        <div v-if = "!burger" @click = "switch_burger" class = "max-sm:flex max-sm:flex-col max-sm:mr-8 max-sm:justify-between max-sm:items-center max-sm:w8 max-sm:h6">
+        <div v-if = "!burger" @click = "switch_burger" class="max-sm:flex max-sm:flex-col max-sm:mr-8 max-sm:mt-4 max-sm:justify-between max-sm:items-center max-sm:w-8 max-sm:h-6">
             <span class = "h-[3px] w-full bg-black"></span>
             <span class = "h-[3px] w-full bg-black"></span>
             <span class = "h-[3px] w-full bg-black"></span>
         </div>
-        <div v-else class = "max-sm:flex max-sm:flex-col max-sm:mr-8 max-sm:justify-between max-sm:items-center max-sm:w8 max-sm:h6" @click="switch_burger">
-            <span class = "h-[3px] w-full bg-black rotate-45 relative top[9px]"></span>
+        <div v-else @click="switch_burger" class = "max-sm:flex max-sm:flex-col max-sm:mr-8 max-sm:mt-4 max-sm:justify-between max-sm:items-center max-sm:w-8 max-sm:h-6">
+            <span class = "h-[3px] w-full bg-black rotate-[45deg] relative top-[12px]"></span>
             <span class = "h-[3px] w-full bg-black opacity-0"></span>
-            <span class = "h-[3px] w-full bg-black relative bottom-3 rotate-[-45deg]"></span>
+            <span class = "h-[3px] w-full bg-black rotate-[-45deg] relative bottom-3"></span>
         </div>
     </header>
     
